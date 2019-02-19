0cb5453859d2383ed266e71e8533a17aaeff2f2b
Dilshat Hewzulla <hewzulla@gmail.com>
2019-02-19 22:06:58 +0000

    added global input app transfer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,131 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import ClipboardCopyButton from '@bit/globalinput.web.clipboard-copy-button';
+import GlobalInputConnect from '@bit/globalinput.web.global-input-connect';
+import {styles} from "./styles";
+
+const textContent={
+    title:"Content Transfer Example",
+    githuburl:"https://github.com/global-input/content-transfer-example"
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  constructor(props){
+      super(props);
+      this.state={content:"", connected:false, notificationMessage:null};
+      this.messageTimeoutHandler=null;
+      this.initMobileData(props);
+  }
+  componentWillUnmount(){
+    if(this.messageTimeoutHandler){
+      clearTimeout(this.messageTimeoutHandler);
+      this.messageTimeoutHandler=null;
+    }
+  }
+  setContent(content){
+      this.setState(Object.assign({},{content}));
+      this.mobile.setContent(content);
+  }
+  setNotificationMessage(notificationMessage){
+    this.setState(Object.assign({},this.state,{notificationMessage}),()=>{
+      this.clipboardTimerHandler=setTimeout(()=>{
+            this.setState(Object.assign({},this.state,{notificationMessage:null}));
+      },2000);
+    });
+  }
+  onSenderConnected(){
+      this.setState(Object.assign({},{connected:true}));
+  }
+  initMobileData(props){
+            this.mobile={
+                  globalInputConnect:null,
+                  sendMessage:(message, fieldid)=>{
+                        if(this.mobile.globalInputConnect){
+
+                              this.mobile.globalInputConnect.sendInputMessage(message,null,fieldid);
+                        }
+                        else{
+
+                        }
+                  },
+                  config:{
+                                url:props.url,
+                                apikey:props.apikey,
+                                securityGroup:props.securityGroup,
+                                initData:{
+                                    action:"input",
+                                    dataType:"control",
+                                    form:{
+                                      title:"Content Transfer",
+                                      fields:[]
+                                    }
+                                },
+                                onSenderConnected:()=>this.onSenderConnected.bind(this)
+                  },
+                  addField: field=>this.mobile.config.initData.form.fields.push(field)
+            };
+            const contentField={
+                    label:"Content",
+                    id:"content",
+                    value:"",
+                    nLines:10,
+                    operations:{
+                        onInput:value=>this.setContent(value)
+                    }
+            };
+            this.mobile.addField(contentField);
+            this.mobile.setContent=content=>{
+              this.mobile.sendMessage(content,contentField.id);
+            }
+  }
+
+
+  renderCopyButton(){
+    if(this.state.notificationMessage){
+
+    }
+  }
+  render(){
+    return(
+      <div style={styles.container}>
+
+        <div style={styles.title}>
+            {textContent.title}
+        </div>
+        <div style={styles.topControl}>
+              <span style={styles.githuburl}>
+                  <a href={textContent.githuburl} target="_blank">{textContent.githuburl}</a>
+              </span>
+              <ClipboardCopyButton copyFieldId="contentField"/>
+
+        </div>
+        <div style={styles.areaContainer}>
+          <textarea  id="contentField" style={styles.textArea.get()}
+            onChange={(evt) => {
+                this.setContent(evt.target.value);
+
+          }} value={this.state.content}/>
+          <div style={styles.globalConnect}>
+                <GlobalInputConnect mobileConfig={this.mobile.config}
+                  ref={globalInputConnect =>this.mobile.globalInputConnect=globalInputConnect}
+                  connectingMessage="Connecting...."
+                  connectedMessage="Scan with Global Input App">
+
+                  </GlobalInputConnect>
+          </div>
+
+        </div>
+
+
+
       </div>
     );
+
   }
+
+
+
+
 }
 
 export default App;
